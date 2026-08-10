a105d79e6468a981d800fe8f5b2343590aecc40f
rmky <ryoooyu@gmail.com>
2026-08-11 00:03:44 +0900

    feat: initialize performance and input modules

--- src/main/java/com/stardustpvp/StardustPvP.java
+++ src/main/java/com/stardustpvp/StardustPvP.java
@@ -1,7 +1,12 @@
 package com.stardustpvp;
 
+import com.stardustpvp.client.MinimumViewBobbing;
+import com.stardustpvp.client.PerformanceOverlay;
+import com.stardustpvp.config.StardustConfig;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = StardustPvP.MOD_ID, name = StardustPvP.NAME, version = StardustPvP.VERSION, clientSideOnly = true, acceptedMinecraftVersions = "[1.8.9]")
 public final class StardustPvP {
@@ -10,7 +15,13 @@ public final class StardustPvP {
     public static final String VERSION = "0.1.0";
 
     @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        StardustConfig.load(event.getModConfigurationDirectory());
+    }
+
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        // Core initialization will be added incrementally.
+        MinecraftForge.EVENT_BUS.register(new MinimumViewBobbing());
+        MinecraftForge.EVENT_BUS.register(new PerformanceOverlay());
     }
 }
