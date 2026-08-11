b8e3c5f8d50b6bc2c4223ecbaf9ebf9f7eaff2fd
rmky <ryoooyu@gmail.com>
2026-08-11 16:21:15 +0900

    hud: register real mouse input tracking

--- src/main/java/com/stardustpvp/StardustPvP.java
+++ src/main/java/com/stardustpvp/StardustPvP.java
@@ -5,17 +5,17 @@ import com.stardustpvp.core.PerformanceStats;
 import com.stardustpvp.core.StardustConfig;
 import com.stardustpvp.core.StardustHud;
 import com.stardustpvp.hud.ArmorPotionRenderer;
+import com.stardustpvp.hud.CpsInputHandler;
 import com.stardustpvp.hud.CpsRenderer;
 import com.stardustpvp.hud.CpsTracker;
 import com.stardustpvp.hud.CrosshairRenderer;
 import com.stardustpvp.hud.HudElementState;
-import com.stardustpvp.hud.HudKeybind;
 import com.stardustpvp.hud.HudLayoutManager;
-import com.stardustpvp.hud.HudPresetLayout;
 import com.stardustpvp.hud.HudPreset;
+import com.stardustpvp.hud.HudPresetLayout;
+import com.stardustpvp.hud.HudEditorController;
 import com.stardustpvp.hud.HudWidgetCollector;
 import com.stardustpvp.hud.HudWidgetRenderer;
-import com.stardustpvp.hud.HudEditorController;
 import com.stardustpvp.hud.KeystrokesRenderer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -46,7 +46,6 @@ public final class StardustPvP {
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new StardustHud(performanceMonitor));
-
         registerHudElements();
         HudPresetLayout.apply(hudLayout, HudPreset.PVP);
 
@@ -54,6 +53,7 @@ public final class StardustPvP {
         MinecraftForge.EVENT_BUS.register(new HudWidgetRenderer(collector, hudLayout));
         MinecraftForge.EVENT_BUS.register(new KeystrokesRenderer(hudLayout));
         MinecraftForge.EVENT_BUS.register(new CpsRenderer(cpsTracker, hudLayout));
+        MinecraftForge.EVENT_BUS.register(new CpsInputHandler(cpsTracker));
         MinecraftForge.EVENT_BUS.register(new ArmorPotionRenderer(hudLayout));
         MinecraftForge.EVENT_BUS.register(new CrosshairRenderer(hudLayout));
         MinecraftForge.EVENT_BUS.register(new HudEditorController(net.minecraft.client.Minecraft.getMinecraft(), hudLayout));
