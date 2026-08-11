7264d3f84ecc688e0d9ab06f877f07c65b381e28
rmky <ryoooyu@gmail.com>
2026-08-11 13:39:10 +0900

    hud: centralize built-in widget IDs

---
+++ src/main/java/com/stardustpvp/hud/HudWidgetIds.java
@@ -0,0 +1,15 @@
+package com.stardustpvp.hud;
+
+/** Stable IDs used by the built-in HUD widgets. */
+public final class HudWidgetIds {
+    public static final String FPS = "fps";
+    public static final String PING = "ping";
+    public static final String CPS = "cps";
+    public static final String KEYSTROKES = "keystrokes";
+    public static final String ARMOR = "armor";
+    public static final String POTIONS = "potions";
+    public static final String COORDINATES = "coordinates";
+    public static final String CROSSHAIR = "crosshair";
+
+    private HudWidgetIds() {}
+}
